7f7071cd31700ca3cf14e8acf04312499fcf9099
KhadeejaShirinVP <khadeejasvp@moback.com>
2022-10-06 14:24:14 +0100

    Study

---
+++ enumExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)WeekDays.Sunday);
+            Console.WriteLine((int)WeekDays.Monday);
+            Console.WriteLine((int)WeekDays.Tuesday);
+            Console.WriteLine((int)WeekDays.Wednesday);
+            Console.WriteLine((int)WeekDays.Thursday);
+            Console.WriteLine((int)WeekDays.Friday);
+            Console.WriteLine((int)WeekDays.Saturday);
+   
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ enumExample/WeekDays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExample
+{
+    public enum WeekDays
+    {
+        Sunday,
+        Monday,
+        Tuesday=8,
+        Wednesday,
+        Thursday,
+        Friday=34,
+        Saturday,
+    }
+}
